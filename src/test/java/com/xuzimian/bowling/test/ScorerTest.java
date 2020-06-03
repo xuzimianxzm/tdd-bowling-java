5774c87cccb959feebb5cbd3a42576b4848e337d
Zimian Xu <zimian.xu@thoughtworks.com>
2020-06-03 21:58:43 +0800

    1. add tasking

---
+++ src/test/java/com/xuzimian/bowling/test/ScorerTest.java
@@ -0,0 +1,26 @@
+package com.xuzimian.bowling.test;
+
+import org.junit.jupiter.api.Test;
+
+public class ScorerTest {
+
+    @Test
+    void when_In_a_round_given_Throw_the_ball_twice_Did_not_knock_down_the_bottles_then_zero() {
+
+    }
+
+    @Test
+    void when_In_a_round_given_throw_the_ball_twice_Did_not_knock_down_all_bottles_then_Number_of_bottles_poured() {
+
+    }
+
+    @Test
+    void when_In_a_round_given_throw_first_is_STRIKE_then_ten_plus_the_number_of_inverted_bottles_tossed_in_the_next_two() {
+
+    }
+
+    @Test
+    void when_In_a_round_given_throw_secon_ball_knock_down_all_bottles_then_ten_plus_the_number_of_inverted_bottles_tossed_in_the_next() {
+
+    }
+}
---
+++ tasking.md
@@ -0,0 +1,12 @@
+### Taskings
+
+1.  when : In a round , given : Throw the ball twice Did not knock down the bottles ，then : zero 
+
+2.  when : In a round , given : throw the ball twice Did not knock down all bottles ，then : Number of bottles poured.
+
+3.  when : In a round , given : throw first is STRIKE ，then : ten plus the number of inverted bottles tossed in the
+ next two
+
+4.  when : In a round , given : throw second ball knock down all bottles ，then : ten plus the number of inverted
+ bottles tossed in the next
+ 
