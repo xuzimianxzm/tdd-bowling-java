c7d539dc7600bda44d475791dc7f69d88b2650b0
Zimian Xu <zimian.xu@thoughtworks.com>
2020-06-03 22:09:26 +0800

    1. 实现测试用例1：when_In_a_round_given_Throw_the_ball_twice_Did_not_knock_down_the_bottles_then_zero

--- src/test/java/com/xuzimian/bowling/test/ScorerTest.java
+++ src/test/java/com/xuzimian/bowling/test/ScorerTest.java
@@ -1,12 +1,19 @@
 package com.xuzimian.bowling.test;
 
+import com.xuzimian.bowling.Scorer;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ScorerTest {
 
     @Test
     void when_In_a_round_given_Throw_the_ball_twice_Did_not_knock_down_the_bottles_then_zero() {
+        int first = 3, second = 5;
 
+        int fraction = Scorer.score(first, second, null);
+
+        assertEquals(first + second, fraction);
     }
 
     @Test
