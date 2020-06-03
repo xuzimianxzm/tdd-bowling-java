0a344cd2cfeac8d6eeaab40a358c97ddf37a5e0f
Zimian Xu <zimian.xu@thoughtworks.com>
2020-06-03 22:18:44 +0800

    1. 实现测试用例2,3,4

--- src/test/java/com/xuzimian/bowling/test/ScorerTest.java
+++ src/test/java/com/xuzimian/bowling/test/ScorerTest.java
@@ -11,27 +11,35 @@ public class ScorerTest {
     void when_In_a_round_given_Throw_the_ball_twice_Did_not_knock_down_the_bottles_then_zero() {
         int firstKnockdownNum = 0, secondKnockdownNum = 0;
 
-        int fraction = Scorer.score(firstKnockdownNum, secondKnockdownNum, null);
+        int actual = Scorer.score(firstKnockdownNum, secondKnockdownNum, null);
 
-        assertEquals(firstKnockdownNum + secondKnockdownNum, fraction);
+        assertEquals(0, actual);
     }
 
     @Test
     void when_In_a_round_given_throw_the_ball_twice_Did_not_knock_down_all_bottles_then_Number_of_bottles_poured() {
         int firstKnockdownNum = 3, secondKnockdownNum = 5;
 
-        int fraction = Scorer.score(firstKnockdownNum, secondKnockdownNum, null);
+        int actual = Scorer.score(firstKnockdownNum, secondKnockdownNum, null);
 
-        assertEquals(firstKnockdownNum + secondKnockdownNum, fraction);
+        assertEquals(firstKnockdownNum + secondKnockdownNum, actual);
     }
 
     @Test
     void when_In_a_round_given_throw_first_is_STRIKE_then_ten_plus_the_number_of_inverted_bottles_tossed_in_the_next_two() {
+        int strike = 10, secondKnockdownNum = 5, thirdKnockdownNum = 8;
 
+        int actual = Scorer.score(strike, secondKnockdownNum, thirdKnockdownNum);
+
+        assertEquals(strike + secondKnockdownNum + thirdKnockdownNum, actual);
     }
 
     @Test
-    void when_In_a_round_given_throw_secon_ball_knock_down_all_bottles_then_ten_plus_the_number_of_inverted_bottles_tossed_in_the_next() {
+    void when_In_a_round_given_throw_second_ball_knock_down_all_bottles_then_ten_plus_the_number_of_inverted_bottles_tossed_in_the_next() {
+        int firstKnockdownNum = 4, secondKnockdownNum = 6, thirdKnockdownNum = 3;
 
+        int actual = Scorer.score(firstKnockdownNum, secondKnockdownNum, thirdKnockdownNum);
+
+        assertEquals(firstKnockdownNum + secondKnockdownNum + thirdKnockdownNum, actual);
     }
 }
